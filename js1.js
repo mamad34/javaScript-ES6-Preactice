1d6899edcdd6e280f181de92de2a94b6bdbc81c7
mohammad <mohammadtt77@yahoo.com>
2021-10-01 22:57:46 +0330

    first js file added

---
+++ js1.js
@@ -0,0 +1,10 @@
+function employee() {
+  const name = "harry";
+  const age = 21;
+  const position = "manager";
+  console.log(`${name} is ${age} and his positon is ${position}`);
+}
+
+const names = ["first", "second", "third"];
+
+console.log(`the name of first array is ${names[0]}`);
