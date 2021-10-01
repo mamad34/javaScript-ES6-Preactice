6474fe60be2314ea19cdc9caf258b1072b1ca321
mohammad <mohammadtt77@yahoo.com>
2021-10-01 23:10:58 +0330

    adde some js variable

--- js1.js
+++ js1.js
@@ -7,4 +7,13 @@ function employee() {
 
 const names = ["first", "second", "third"];
 
+const jsVariableCar = {
+  gearbox: "manual",
+  cc: "1600cc",
+};
+
 console.log(`the name of first array is ${names[0]}`);
+console.log(`5+1 is  ${5 + 1}`);
+console.log(
+  `js object car cc is  ${jsVariableCar.cc} and gearbox is : ${jsVariableCar.gearbox}`
+);
