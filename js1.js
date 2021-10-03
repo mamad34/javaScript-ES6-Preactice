34d836c22ff3915ce8e530fd3f1701838d40df7f
mohammad <mohammadtt77@yahoo.com>
2021-10-03 21:39:07 +0330

    start filter helper

--- js1.js
+++ js1.js
@@ -45,3 +45,37 @@ purchases.forEach(function (item) {
 });
 
 console.log(total);
+
+// start Map
+
+/*
+
+var number = [0, 1, 2, 3, 4, 5, 6];
+
+var otherArray = [];
+
+for (i = 0; i < number.length; i++) {
+  otherArray.push(number[i] * 10);
+}
+console.log(otherArray);
+ */
+
+const numbers = [0, 1, 2, 3, 4, 5];
+
+const otherArray = numbers.map(function (num) {
+  return num * 10;
+});
+console.log(otherArray);
+// map 2
+const users = [
+  { user: "mamad", age: "22", eyes: "brown" },
+  { user: "mamad2", age: "23", eyes: "brown2" },
+  { user: "mamad3", age: "24", eyes: "brown3" },
+];
+
+const listOfUsers = users.map(function (item) {
+  return item.user;
+});
+console.log(listOfUsers);
+
+//filter helper start
