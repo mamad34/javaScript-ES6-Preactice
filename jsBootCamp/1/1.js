9216c94521632107e57706ef9fdaa3307f6d8868
Mamad <mohammadtt77@yahoo.com>
2021-11-29 13:45:47 +0330

    switch case

---
+++ jsBootCamp/1/1.js
@@ -0,0 +1,38 @@
+let day = 33;
+
+switch (day) {
+    case 1:
+        {
+            console.log("satarday");
+            break;
+        }
+    case 2:
+        {
+            console.log("sunday");
+            break;
+        }
+    case 3:
+        {
+            console.log("monday");
+            break;
+        }
+    case 4:
+        {
+            console.log("thursday");
+            break;
+        }
+    case 5:
+        {
+            console.log("wendsday");
+            break;
+        }
+    case 6:
+        {
+            console.log("thuerqwewq");
+            break;
+        }
+    default:
+        {
+            console.log("oh yea");
+        }
+}
