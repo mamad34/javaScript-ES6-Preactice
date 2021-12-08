3f3d3c05863d654ac3bcd0f7af59cae91d1b49f4
Mamad <mohammadtt77@yahoo.com>
2021-12-08 16:56:18 +0330

    find

--- jsBootCamp/2/2.js
+++ jsBootCamp/2/2.js
@@ -142,3 +142,91 @@ console.log(hoot); // undefined
 var hoot = function() {
     console.log("HOOO HOOOOO");
 };
+
+//forEach
+
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27, 28];
+
+numbers.forEach(function(num) {
+    console.log(num);
+});
+
+function printDouble(x) {
+    console.log(x * 2);
+}
+numbers.forEach(printDouble);
+
+numbers.forEach(function(number, index) {
+    console.log(index, number);
+});
+
+// map
+
+const doubles = numbers.map(function(num) {
+    return num * 2; // map need return
+});
+
+const numDetail = numbers.map(function(n) {
+    return {
+        value: n,
+        isEven: n % 2 === 0,
+    };
+});
+
+const words = ["asap", "byob", "rsvp", "diy"];
+const splitAndJoined = words.map(function(word) {
+    return word.toUpperCase().split("").join(".");
+});
+
+// Arrow functions
+
+const square = (x) => {
+    return x * x;
+};
+
+//or
+// const square2 = x => {
+//     return x * x;
+// };
+
+const multiply = (x, y) => {
+    return x * y;
+};
+
+const homYea = () => {
+    console.log("HOOOOOM YEAAAAA");
+};
+
+// this type of arrow functions dose not need to use return keyword
+
+//  const twoSome = (n) => (
+//     n * 2;
+//     )
+const twoSome = (n) => n * 2;
+
+const flouts = [1.1, 2.2, 33.3, 4.4, 2.6, 68.9];
+
+const newFlouts = flouts.map((n) => {
+    return n / 2;
+});
+
+const newFlouts2 = flouts.map((n) => n * 3);
+
+const evenOrOdd = flouts.map((n) => (n % 2 === 0 ? "even" : "odd"));
+
+// Array find
+
+let films = ["ha ye", "ho yea ", " ha no  ", "ho no"];
+
+const film = films.find((movie) => {
+    return movie.includes("ha");
+});
+
+const film2 = films.find((m) => m.indexOf("ha") === 0);
+const film3 = films.find((m) => m.indexOf("ha") === 1);
+
+const bookRating = [1.2, 3.4, 6.6, 8.8, 9.9, 7.6, 4.2, 1.2];
+
+const rates = bookRating.filter((b) => b > 6.6);
+
+//Array Filter
