e93456e44793ade4173f0df0e5137bd2b4b06c3c
Mamad <mohammadtt77@yahoo.com>
2021-12-18 17:13:59 +0330

    computed styles

--- jsBootCamp/5/5.js
+++ jsBootCamp/5/5.js
@@ -124,3 +124,76 @@ console.log(theATag.href);
 const theImage = document.querySelector("img");
 theImage.src =
     "https://images.unsplash.com/photo-1639610806386-661d00972b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+
+// gettin & settin Attributes
+
+const range = document.querySelector('input[type="range"]');
+console.log(range.getAttribute("max"));
+console.log(range.getAttribute("min"));
+range.setAttribute("min", "-500");
+console.log(range.getAttribute("min"));
+
+const checkbox = document.querySelector('input[type="checkbox"]');
+checkbox.setAttribute("type", "radio");
+
+const firstLI = document.querySelector("li");
+console.dir(firstLI);
+console.dir(firstLI.parentElement);
+console.dir(firstLI.parentElement.parentElement);
+console.dir(firstLI.parentElement.parentElement.parentElement);
+console.dir(firstLI.parentElement.parentElement.parentElement.parentElement);
+
+console.log(ul.children);
+console.log(ul.children[0]);
+console.log(ul.children[0].innerText);
+
+console.log(firstLI.nextElementSibling); // it is second li
+console.log(firstLI.nextElementSibling.nextElementSibling); // it is third li
+
+const thirdLI = firstLI.nextElementSibling.nextElementSibling;
+console.log(thirdLI.previousElementSibling);
+
+// changing Multiple Elements
+
+const allLis = document.querySelectorAll("li");
+for (let li of allLis) {
+    console.dir(li);
+}
+//or
+for (let i = 0; i < allLis.length; i++) {
+    console.log(allLis[i].innerText);
+}
+
+for (let i = 0; i < allLis.length; i++) {
+    allLis[i].innerHTML += "Changed by a <b> loop </b>";
+}
+
+// Altering Styles
+
+console.log(theH1.style);
+console.log(theH1.style.color); // empty // color will only show if we use inline css
+theH1.style.color = "orchid";
+
+const alP = document.querySelector("p");
+
+alP.style.color = "white";
+// we dont have p.style.background-color
+alP.style.backgroundColor = "black";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+allLis.forEach((element, index) => {
+    const color = colors[index];
+    console.log(element, index);
+    console.log(element, color);
+    element.style.color = color;
+});
+
+const li = document.querySelector("li"); // we cant get the styles from li becuse its just find styles in inline css
+// for that we do this ------>>>>
+const liStyle = getComputedStyle(li);
+console.log(liStyle);
+console.log(liStyle.color);
+
+compStyles = getComputedStyle(theH1);
+console.log(compStyles.color);
+console.log(compStyles.fontSize);
